62bf2565dabc628b7d7a987ae344fa73b5dd347f
Luluuou <svetok.nastya@gmail.com>
2025-11-17 17:06:12 +0200

    Update api_views.py

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -52,3 +52,11 @@ def add_opinion():
     # Сохранить изменения.
     db.session.commit()
     return jsonify({'opinion': opinion.to_dict()}), 201 
+
+@app.route('/api/get-random-opinion/', methods=['GET'])
+def get_random_opinion():
+    quantity = Opinion.query.count()
+    if quantity:
+        offset_value = randrange(quantity)
+        opinion = Opinion.query.offset(offset_value).first()
+        return jsonify({'opinion': opinion.to_dict()}), 200 
