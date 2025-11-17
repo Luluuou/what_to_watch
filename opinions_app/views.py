33fd282775d2197eabf60fd7c781c062803cc90a
Luluuou <svetok.nastya@gmail.com>
2025-11-17 17:15:52 +0200

    Update views.py

--- opinions_app/views.py
+++ opinions_app/views.py
@@ -5,6 +5,7 @@ from flask import abort, flash, redirect, render_template, url_for
 from . import app, db
 from .forms import OpinionForm
 from .models import Opinion
+from .dropbox import upload_files_to_dropbox
 
 def random_opinion():
     quantity = Opinion.query.count()
@@ -24,16 +25,22 @@ def index_view():
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_opinion_view():
-    form = OpinionForm()
+    form = OpinionForm()    
     if form.validate_on_submit():
         text = form.text.data
         if Opinion.query.filter_by(text=text).first() is not None:
             flash('Такое мнение уже было оставлено ранее!')
             return render_template('add_opinion.html', form=form)
+        # Добавьте вызов функции загрузки файлов 
+        # и передайте туда сами файлы.
+        urls = upload_files_to_dropbox(form.images.data)
         opinion = Opinion(
             title=form.title.data, 
             text=text, 
-            source=form.source.data
+            source=form.source.data,
+            # При создании объекта передайте все ссылки 
+            # на изображения в поле images.
+            images=urls
         )
         db.session.add(opinion)
         db.session.commit()
