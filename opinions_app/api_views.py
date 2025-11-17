b4b9de672e887cfcc449c1fa1b2912d9a91fd1a8
Luluuou <svetok.nastya@gmail.com>
2025-11-17 17:03:16 +0200

    Update api_views.py

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -1,15 +1,23 @@
 # what_to_watch/opinions_app/api_views.py
 
-# Импортировать метод jsonify.
-from flask import jsonify  
+from flask import jsonify
 
 from . import app
 from .models import Opinion
 
-# Явно разрешить метод GET.
-@app.route('/api/opinions/<int:id>/', methods=['GET'])  
+def opinion_to_dict(opinion):
+    return dict(
+        id = opinion.id,
+        title = opinion.title,
+        text = opinion.text,
+        source = opinion.source,
+        timestamp = opinion.timestamp,
+        added_by = opinion.added_by
+    )
+
+@app.route('/api/opinions/<int:id>/', methods=['GET'])
 def get_opinion(id):
-    # Получить объект по id или выбросить ошибку 404.
     opinion = Opinion.query.get_or_404(id)
-    # Конвертировать данные в JSON и вернуть JSON-объект и HTTP-код ответа.
-    return jsonify({'opinion': opinion}), 200
+    # Конвертировать объект модели в словарь.
+    data = opinion_to_dict(opinion)  
+    return jsonify({'opinion': data}), 200
