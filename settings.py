51752c5676b35b4a0d8eb67fe1cfe450d9494c66
Luluuou <svetok.nastya@gmail.com>
2025-11-17 17:14:09 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,9 @@
+# what_to_watch/settings.py
+
 import os
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
-    SECRET_KEY = os.getenv('SECRET_KEY') 
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    # Добавьте строчку для получения токена из переменных окружения.
+    DROPBOX_TOKEN = os.getenv('DROPBOX_TOKEN')
