ef06b5eed360a8d3f5cc0c6d024840772e5ab56d
Luluuou <svetok.nastya@gmail.com>
2025-11-17 17:07:20 +0200

    Update api_views.py

--- opinions_app/api_views.py
+++ opinions_app/api_views.py
@@ -4,59 +4,46 @@ from flask import jsonify, request
 
 from . import app, db
 from .models import Opinion
+from .views import random_opinion
 
-@app.route('/api/opinions/', methods=['GET'])
-def get_opinions():
-    # Запросить список объектов.
-    opinions = Opinion.query.all()
-    # Поочерёдно сериализовать каждый объект,
-    # а потом все объекты поместить в список opinions_list.
-    opinions_list = [opinion.to_dict() for opinion in opinions]
-    return jsonify({'opinions': opinions_list}), 200 
+@app.route('/api/opinions/<int:id>/', methods=['GET'])
+def get_opinion(id):
+    opinion = Opinion.query.get_or_404(id)
+    return jsonify({'opinion': opinion.to_dict()}), 200
 
 @app.route('/api/opinions/<int:id>/', methods=['PATCH'])
 def update_opinion(id):
-    data = request.get_json()
-    # Если метод get_or_404 не найдёт указанный ID, 
-    # он выбросит исключение 404:
     opinion = Opinion.query.get_or_404(id)
-    opinion.title = data.get('title', opinion.title)
-    opinion.text = data.get('text', opinion.text)
-    opinion.source = data.get('source', opinion.source)
-    opinion.added_by = data.get('added_by', opinion.added_by)
-    # Все изменения нужно сохранить в базе данных.
-    # Объект opinion добавлять в сессию не нужно.
-    # Этот объект получен из БД методом get_or_404() и уже хранится в сессии.
+    opinion.title = request.json.get('title', opinion.title)
+    opinion.text = request.json.get('text', opinion.text)
+    opinion.source = request.json.get('source', opinion.source)
+    opinion.added_by = request.json.get('added_by', opinion.added_by)
     db.session.commit()  
-    # При изменении объекта нужно вернуть сам объект и код 200.
-    return jsonify({'opinion': opinion.to_dict()}), 200
+    return jsonify({'opinion': opinion.to_dict()}), 201
 
 @app.route('/api/opinions/<int:id>/', methods=['DELETE'])
 def delete_opinion(id):
     opinion = Opinion.query.get_or_404(id)
     db.session.delete(opinion)
     db.session.commit()
-    # При удалении принято возвращать только код ответа 204.
-    return '', 204   
+    return '', 204
+
+@app.route('/api/opinions/', methods=['GET'])
+def get_opinions():
+    opinions = Opinion.query.all()  
+    opinions_list = [opinion.to_dict() for opinion in opinions]
+    return jsonify({'opinions': opinions_list}), 200
 
 @app.route('/api/opinions/', methods=['POST'])
 def add_opinion():
-    # Получить данные из запроса в виде словаря.
-    data = request.get_json()
-    # Создать новый пустой экземпляр модели.
-    opinion = Opinion()
-    # Наполнить экземпляр данными из запроса.
-    opinion.from_dict(data)
-    # Добавить новую запись в сессию.
-    db.session.add(opinion)
-    # Сохранить изменения.
-    db.session.commit()
-    return jsonify({'opinion': opinion.to_dict()}), 201 
+    data = request.get_json()  
+    opinion = Opinion()  
+    opinion.from_dict(data)  
+    db.session.add(opinion)  
+    db.session.commit()  
+    return jsonify({'opinion': opinion.to_dict()}), 201
 
 @app.route('/api/get-random-opinion/', methods=['GET'])
 def get_random_opinion():
-    quantity = Opinion.query.count()
-    if quantity:
-        offset_value = randrange(quantity)
-        opinion = Opinion.query.offset(offset_value).first()
-        return jsonify({'opinion': opinion.to_dict()}), 200 
+    opinion = random_opinion()
+    return jsonify({'opinion': opinion.to_dict()}), 200
