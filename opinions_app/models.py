2f14aa398d27373e7734a03ef54979487a614b11
Luluuou <svetok.nastya@gmail.com>
2025-11-17 17:03:30 +0200

    Update models.py

--- opinions_app/models.py
+++ opinions_app/models.py
@@ -1,4 +1,6 @@
-from datetime import datetime
+# what_to_watch/opinions_app/models.py
+
+from datetime import datetime, UTC
 
 from . import db
 
@@ -7,5 +9,16 @@ class Opinion(db.Model):
     title = db.Column(db.String(128), nullable=False)
     text = db.Column(db.Text, unique=True, nullable=False)
     source = db.Column(db.String(256))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.now(UTC))
     added_by = db.Column(db.String(64))
+
+    # Вот он — новый метод:
+    def to_dict(self):
+        return dict(
+            id = self.id,
+            title = self.title,
+            text = self.text,
+            source = self.source,
+            timestamp = self.timestamp,
+            added_by = self.added_by
+        )
