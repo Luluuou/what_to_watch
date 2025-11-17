5ecfeac408894c465046627fb620eda41b961527
Luluuou <svetok.nastya@gmail.com>
2025-11-17 16:44:02 +0200

    Update opinions_app.py

--- opinions_app.py
+++ opinions_app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from random import randrange
 
 # Импортировать функцию render_template().
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_wtf import FlaskForm
@@ -52,12 +52,26 @@ def index_view():
     return render_template('opinion.html', opinion=opinion)
     
 
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add_opinion_view():
-    # Создать новый экземпляр формы.
     form = OpinionForm()
-    # Передать объект формы в шаблон add_opinion.html.
-    return render_template('add_opinion.html', form=form)
+    # Если ошибок не возникло...
+    if form.validate_on_submit():
+        # ...то нужно создать новый экземпляр класса Opinion...
+        opinion = Opinion(
+            # ...и передать в него данные, полученные из формы.
+            title=form.title.data,
+            text=form.text.data,
+            source=form.source.data
+        )
+        # Затем добавить его в сессию работы с базой данных...
+        db.session.add(opinion)
+        # ...и зафиксировать изменения.
+        db.session.commit()
+        # Затем переадресовать пользователя на страницу добавленного мнения.
+        return redirect(url_for('opinion_view', id=opinion.id))
+    # Если валидация не пройдена - просто отрисовать страницу с формой.
+    return render_template('add_opinion.html', form=form) 
 
 @app.route('/opinions/<int:id>')  
 def opinion_view(id):  
